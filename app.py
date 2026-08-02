62c4e30e9f45fc64b1d95a33ae8975886291b67e
ai-test2 <ai.us2@hotmail.com>
2026-08-02 04:29:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -42,13 +42,6 @@ with st.sidebar:
         }
     )
 
-# 1. Stavljamo vertikalni razmak (gura sve ispod prema dnu, ovisno o količini slobodnog prostora)
-    
-    
-    # 2. Tehnička napomena na dnu
-    st.markdown("---")
-    st.caption("Sistem v1.0 | Streamlit & GitHub Connected")
-
 st.markdown("<br>" * 10, unsafe_allow_html=True) 
 st.sidebar.markdown("---")
 st.sidebar.caption("Sistem v1.0 | Streamlit & GitHub Connected")
